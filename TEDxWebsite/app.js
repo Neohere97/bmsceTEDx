818e74cd8ddb4d5fda6aa52639e998792407b16b
dhruvmetha <dhruv.metha@gmail.com>
2018-07-22 18:05:09 +0530

    Added the server

---
+++ TEDxWebsite/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
+
+let port = process.env.port || 3000;
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+
+app.get("/gallery",(req,res)=>{
+    res.render('gallery');
+});
+
+
+app.get("/about",(req,res)=>{
+    res.render('about');
+});
+
+
+
+app.listen(port,()=>{console.log("Server Started...");});
+
+
+
